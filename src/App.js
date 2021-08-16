1ff7b6e1602d3b391ad759f509320fe36ed2e3f7
Rogelio <rogeliogalicia@Rogelios-MacBook-Air.local>
2021-08-16 01:17:34 -0500

    missing giffs and fix contact info container

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Skills from "./components/Skills";
+
+function App() {
+  return (
+    <main className="text-gray-400 bg-black body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
