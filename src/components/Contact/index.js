b80d978bd6e5270bc27d60599c0680f8af387830
Rogelio <rogeliogalicia@Mac-mini.lan>
2021-10-19 02:32:34 -0500

    centered the contact section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,42 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 function Contact() {
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [message, setMessage] = React.useState("");
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+  const [errorMessage, setErrorMessage] = useState("");
 
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage("Your email is invalid");
+      } else {
+        if (!e.target.value.length) {
+          setErrorMessage(`${e.target.name} is required`);
+        } else {
+          setErrorMessage("");
+        }
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
+    // console.log(formState);
   }
 
+  // JSX
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <form
           name="contact"
           onSubmit={handleSubmit}
-          className="lg:w-3/4 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+          className="lg:w-1/2 md:w-1/2 flex flex-col md:mx-10 w-full md:py-8 mt-8 md:mt-0"
         >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Contact Me
           </h2>
           <p className="leading-relaxed mb-5">
-            If you have any questions or would like to hire me!
+            If you have any questions or would like to work with me!
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
@@ -44,10 +56,10 @@ function Contact() {
             </label>
             <input
               type="text"
-              id="name"
+              defaultValue={name}
               name="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-red-500 focus:ring-2 focus:ring-red-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
+              onBlur={handleChange}
             />
           </div>
           <div className="relative mb-4">
@@ -56,10 +68,10 @@ function Contact() {
             </label>
             <input
               type="email"
-              id="email"
+              defaultValue={email}
               name="email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-red-500 focus:ring-2 focus:ring-red-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
+              onBlur={handleChange}
             />
           </div>
           <div className="relative mb-4">
@@ -70,12 +82,17 @@ function Contact() {
               Message
             </label>
             <textarea
-              id="message"
+              defaultValue={message}
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-red-500 focus:ring-2 focus:ring-red-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setMessage(e.target.value)}
+              onBlur={handleChange}
             />
           </div>
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
           <button
             type="submit"
             className="text-white bg-red-900 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg"
