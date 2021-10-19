7beccaa1af1c61c7ce45990fa50c91bbcf581c86
Rogelio <rogeliogalicia@Mac-mini.lan>
2021-10-18 21:27:27 -0500

    images are displaying now

--- src/components/Data/index.js
+++ src/components/Data/index.js
@@ -1,35 +1,40 @@
+
 export const projects = [
   {
     title: "What's for Dinner",
     subtitle: "Third Party API Application",
     description:
       "An application that will allow the user to get ideas for meals by searching for recipes either by category or keyword.",
-    image: "../../assets/gifs/wfd.gif",
+    image: "whats-for-dinner",
     link: "https://nicopetelo.github.io/whats-for-dinner/",
+    github: "https://github.com/GaliciaRogelio/whats-for-dinner"
   },
   {
     title: "The Goodvice Blog",
     subtitle: "Interactive Full-Stack Application",
     description:
       "A small scale app, enabling users to get advice from their community on everyday things and allowing them to quickly search by categories or keywords.",
-    image: "../../assets/gifs/tgvb.gif",
+    image: "the-goodVice-blog",
     link: "https://the-goodvice-blog.herokuapp.com/",
+    github: "https://github.com/GaliciaRogelio/the-goodvice-blog"
   },
   {
     title: "Budget Tracker",
     subtitle: "Progresive Web Application (PWA)",
     description:
       "This is a budget tracker application that allows offline access and functionality. The user is able to add expenses and deposits to their budget with or without internet connection.",
-    image: "../../assets/gifs/bt.gif",
+    image: "budget-tracker",
     link: "https://rojos-budget-tracker.herokuapp.com/",
+    github: "https://github.com/GaliciaRogelio/my-budget-tracker"
   },
   {
-    title: "Your City Weather App",
-    subtitle: "Third Party API",
+    title: "Shop-Shop",
+    subtitle: "Full Stack E-commerce",
     description:
-      "This is a weather application where users are able to search a city and the app will look through a third party API for that specific city and return results incluiding humidity, wind speed, and UV index",
-    image: "../../assets/gifs/ycw.gif",
-    link: "https://galiciarogelio.github.io/your-city-weather/",
+      "Full Stack E-commerce with the ability to login, signup, and logout. You can add items to your cart and they will be saved for later using local storage. ",
+    image: "shop-shop",
+    link: "https://my-shop-shop.herokuapp.com/",
+    github: "https://github.com/GaliciaRogelio/shop-shop"
   },
 ];
 
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,8 +1,6 @@
 import React from "react";
-
 import { CodeIcon } from "@heroicons/react/solid";
 import { projects } from "../Data";
-
 function Projects() {
   return (
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
@@ -22,8 +20,7 @@ function Projects() {
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
-                  key={project.title}
-                  src={project.image}
+                  src={require(`../../assets/${project.image}.png`).default}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-red-900 mb-1">
