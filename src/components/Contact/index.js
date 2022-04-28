77c0f8ceb0ebde33feb19dd30dc40c35245bfff8
Rogelio <galiciar0513@gmail.com>
2022-04-27 21:06:37 -0500

    added contact information

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -38,7 +38,7 @@ function Contact() {
   // JSX
   return (
     <section id="contact" className="relative">
-      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+      <div className="container justify-center px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <form
           name="contact"
           onSubmit={handleSubmit}
@@ -47,9 +47,7 @@ function Contact() {
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Contact Me
           </h2>
-          <p className="leading-relaxed mb-5">
-            If you have any questions or would like to work with me!
-          </p>
+          <p className="leading-relaxed mb-5">Questions? let's chat!</p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
               Name
@@ -64,13 +62,14 @@ function Contact() {
           </div>
           <div className="relative mb-4">
             <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-              Email
+              Email address
             </label>
             <input
               type="email"
               defaultValue={email}
               name="email"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-red-500 focus:ring-2 focus:ring-red-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              placeholder="your.email@example.com"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-red-500 focus:ring-2 focus:ring-red-900 focus:ring-opacity-50 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               onBlur={handleChange}
             />
           </div>
@@ -100,6 +99,80 @@ function Contact() {
             Submit
           </button>
         </form>
+
+        <div class="lg:w-1/2 md:w-1/2 p-20 mr-2 dark:bg-gray-800">
+          <div class="flex items-center mt-8 text-gray-400 dark:text-gray-400">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              class="w-8 h-8 text-gray-500"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+            <div class="ml-4 text-md tracking-wide font-semibold w-40">
+              Chicago, IL
+            </div>
+          </div>
+
+          <div class="flex items-center mt-4 text-gray-400 dark:text-gray-400">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              class="w-8 h-8 text-gray-500"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+              />
+            </svg>
+            <div class="ml-4 text-md tracking-wide font-semibold w-40">
+              (415) 224-9808
+            </div>
+          </div>
+
+          <div class="flex items-center mt-2 text-gray-400 dark:text-gray-400">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              class="w-8 h-8 text-gray-500"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+              />
+            </svg>
+            <div class="ml-4 text-md tracking-wide font-semibold w-40">
+              galiciar0513@gmail.com
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
